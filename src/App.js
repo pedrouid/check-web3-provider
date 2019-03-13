68f9bf7c378d205460a0ec6af9c15b69eb245f7e
Pedro Gomes <pedrogomes94@gmail.com>
2019-03-13 15:42:33 +0000

    push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import pass from "./assets/pass.png";
+import fail from "./assets/fail.png";
+
+const SApp = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const SProvider = styled.div`
+  width: 100%;
+  max-width: 600px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SProviderName = styled.div`
+  font-weight: bold;
+`;
+
+const SProviderCheck = styled.div`
+  width: 40px;
+  height: 40px;
+  & img {
+    width: 100%;
+  }
+`;
 
 class App extends Component {
+  state = {
+    injectedWeb3: window.ethereum || window.web3,
+    isMetaMask: window.web3 ? window.web3.currentProvider.isMetaMask : false,
+    isTrust: window.web3 ? window.web3.currentProvider.isTrust : false,
+    isToshi: window.web3 ? window.web3.currentProvider.isToshi : false,
+    isCipher: window.web3 ? window.web3.currentProvider.isCipher : false,
+    isImToken: window.web3 ? window.web3.currentProvider.isImToken : false,
+    isStatus: window.web3 ? window.web3.currentProvider.isStatus : false,
+    isTokenary: window.web3 ? window.web3.currentProvider.isTokenary : false
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <SApp>
+        <SProvider>
+          <SProviderName>{"Web3 Available"}</SProviderName>
+          <SProviderCheck>
+            <img src={this.state.injectedWeb3 ? pass : fail} alt="check" />
+          </SProviderCheck>
+        </SProvider>
+
+        <SProvider>
+          <SProviderName>{"Metamask"}</SProviderName>
+          <SProviderCheck>
+            <img src={this.state.isMetaMask ? pass : fail} alt="check" />
+          </SProviderCheck>
+        </SProvider>
+
+        <SProvider>
+          <SProviderName>{"Trust"}</SProviderName>
+          <SProviderCheck>
+            <img src={this.state.isTrust ? pass : fail} alt="check" />
+          </SProviderCheck>
+        </SProvider>
+
+        <SProvider>
+          <SProviderName>{"Coinbase"}</SProviderName>
+          <SProviderCheck>
+            <img src={this.state.isToshi ? pass : fail} alt="check" />
+          </SProviderCheck>
+        </SProvider>
+
+        <SProvider>
+          <SProviderName>{"Cipher"}</SProviderName>
+          <SProviderCheck>
+            <img src={this.state.isCipher ? pass : fail} alt="check" />
+          </SProviderCheck>
+        </SProvider>
+
+        <SProvider>
+          <SProviderName>{"imToken"}</SProviderName>
+          <SProviderCheck>
+            <img src={this.state.isImToken ? pass : fail} alt="check" />
+          </SProviderCheck>
+        </SProvider>
+
+        <SProvider>
+          <SProviderName>{"Status"}</SProviderName>
+          <SProviderCheck>
+            <img src={this.state.isStatus ? pass : fail} alt="check" />
+          </SProviderCheck>
+        </SProvider>
+
+        <SProvider>
+          <SProviderName>{"Tokenary"}</SProviderName>
+          <SProviderCheck>
+            <img src={this.state.isTokenary ? pass : fail} alt="check" />
+          </SProviderCheck>
+        </SProvider>
+      </SApp>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createGlobalStyle } from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { globalStyle } from "./styles";
+
+const GlobalStyle = createGlobalStyle`
+  ${globalStyle}
+`;
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById("root")
+);
